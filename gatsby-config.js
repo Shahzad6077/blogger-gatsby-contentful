e84f07666e1c597c0eda87f5d85b1327d481533f
shahzad6077 <alim6077@gmail.com>
2021-01-18 00:07:15 +0500

    Change env Names

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,21 +64,19 @@ module.exports = {
       resolve: "gatsby-plugin-firebase",
       options: {
         credentials: {
-          apiKey: process.env.apiKey,
-          authDomain: process.env.authDomain,
-          databaseURL: process.env.databaseURL,
-          projectId: process.env.projectId,
-          storageBucket: process.env.storageBucket,
-          messagingSenderId: process.env.messagingSenderId,
-          appId: process.env.appId,
+          apiKey: process.env.FB_API_KEY,
+          authDomain: process.env.FB_AUTH_DOMAIN,
+          databaseURL: process.env.FB_DATABASE_URL,
+          projectId: process.env.FB_PROJECT_ID,
+          appId: process.env.FB_APP_ID,
         },
       },
     },
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.contentful_spaceId,
-        accessToken: process.env.contentful_accessToken,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
   ],
